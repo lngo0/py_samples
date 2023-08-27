7cd606957bcde3f38c712ff8247b8e60967619a3
lngo0 <longngo.gm@gmail.com>
2023-08-27 15:58:41 -0500

    intro sets

---
+++ introSets.py
@@ -0,0 +1,10 @@
+def average(array):
+    # your code goes here
+    new_arr = set(array)
+    return sum(new_arr) / len(new_arr)
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
