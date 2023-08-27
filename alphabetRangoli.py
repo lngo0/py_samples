82652e890ff76d9eeda94d216b541a07e5505814
lngo0 <longngo.gm@gmail.com>
2023-08-27 16:28:10 -0500

    alphabet rangoli

---
+++ alphabetRangoli.py
@@ -0,0 +1,15 @@
+def print_rangoli(size):
+    n = size
+    l1 = list(map(chr, range(97, 123)))
+    x = l1[n-1::-1] + l1[1:n]
+    m = len('-'.join(x))
+    # first 5 rows
+    for i in range(1, n):
+        print('-'.join(l1[n-1:n-i:-1] + l1[n-i:n]).center(m, '-'))
+    # mid and last 5 rows
+    for i in range(n, 0, -1):
+        print('-'.join(l1[n-1:n-i:-1] + l1[n-i:n]).center(m, '-'))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
