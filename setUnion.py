56315599b2d58049f2811a04217d6e25755af2ed
lngo0 <longngo.gm@gmail.com>
2023-08-28 17:12:29 -0500

    set union

---
+++ setUnion.py
@@ -0,0 +1,10 @@
+n1 = int(input())
+s1 = set(map(int, input().split()))
+n2 = int(input())
+s2 = set(map(int, input().split()))
+s3 = s1.union(s2)
+count = 0
+for i in s3:
+    count += 1
+print(count)
+
